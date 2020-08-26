215547ec4163dfadb49324a1b5d090856a0befb0
CocoaPebble <jyz18221816395@gmail.com>
2020-08-25 21:08:48 -0400

    -Fix type in and calculate big number\n

--- js/style.js
+++ js/style.js
@@ -18,30 +18,46 @@ class Calculator {
       let op = this.currentOperand;
       op = op.replace('×', '*').replace('÷', '/');
       var value = mexp.eval(op);
-      // console.log(op);
+      console.log(op);
       console.log(value);
 
+      if (value.length >= 12) {
+        alert("Result is too big");
+        return;
+      }
+
       if (value === 'Infinity') {
-        throw (alert('Cannot divided by 0!!!'))
+        alert('Cannot divided by 0!!!');
+        return;
       }
 
       this.currentOperand = value;
       this.hasCalculated = true;
 
     } catch (error) {
-      alert('Expression error!')
-      this.init()
+      alert('Expression error!');
+      this.init();
     }
   }
 
   appendNumber(number) {
     const len = this.currentOperand.length;
 
+    // unable to append an operator first or right after an operator
     if (isNaN(Number(number)) && isNaN(Number(this.currentOperand.charAt(len - 1)))) return;
+
+    // unable to enter number after 12 bits 
+    if (len >= 12) return;
+
+    // unable to assign the first number with 0
     if (this.currentOperand === '' && number === '0') return;
+
+    // clear the result after calculation
     if (this.hasCalculated === true) {
       this.init();
     }
+
+
 
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
@@ -55,6 +71,7 @@ const numberButtons = document.querySelectorAll('[data-number]');
 const equalButtons = document.querySelector('[data-equal]');
 const deleteButtons = document.querySelector('[data-delete]');
 const currentOutput = document.querySelector('[data-output]');
+
 
 const calculator = new Calculator(currentOutput);
 
